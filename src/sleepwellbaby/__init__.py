53af17ef43e451af3e5acbd4f41bad125b0fb9cc
Richard <r.t.bartels-6@umcutrecht.nl>
2025-07-30 17:03:28 +0200

    Add changelog, update version number

--- src/sleepwellbaby/__init__.py
+++ src/sleepwellbaby/__init__.py
@@ -2,7 +2,7 @@ import logging
 from pathlib import Path
 
 logger = logging.getLogger(__name__)
-version = "0.1.0"
+version = "1.0.0"
 
 # TODO: Consider moving the model files to the templates directory within the package
 # https://stackoverflow.com/questions/6028000/how-to-read-a-static-file-from-inside-a-python-package/58941536#58941536
